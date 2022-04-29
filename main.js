c665ec491a68b11dd376fceaee1c0a9c5b4e1841
abduaziz <104622399+abduazizpoytug@users.noreply.github.com>
2022-04-29 14:28:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+    let kopay = 1;
+    let keykopay = 12;
+
+    const javob = kopay + keykopay
+
+
+    if(javob === 13){
+        console.log('ha bu togri')
+    }
+    else if(javob === 17){
+        console.log('notogri javob');
+    }
